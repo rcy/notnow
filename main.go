9006704f2320bb3638d3eff9a9d193c5046650cc
Ryan Yeske <ryan@ryanyeske.com>
2023-11-06 18:45:22 -0700

    Move auth pages

--- main.go
+++ main.go
@@ -3,23 +3,18 @@ package main
 import (
 	"context"
 	_ "embed"
-	"encoding/json"
-	"errors"
 	"html/template"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
+	"yikes/routes/auth"
 
-	"yikes/config"
-	"yikes/db/yikes"
 	ymw "yikes/middleware"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
-	"golang.org/x/oauth2"
 	"google.golang.org/api/calendar/v3"
 )
 
@@ -42,14 +37,14 @@ func main() {
 
 	r.Use(middleware.Logger)
 
-	r.Group(func(r chi.Router) {
-		r.Use(func(next http.Handler) http.Handler {
-			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				ctx := r.Context()
-				next.ServeHTTP(w, r.WithContext(context.WithValue(ctx, "conn", conn)))
-			})
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			next.ServeHTTP(w, r.WithContext(context.WithValue(ctx, "conn", conn)))
 		})
+	})
 
+	r.Group(func(r chi.Router) {
 		r.Use(ymw.User)
 		r.Use(ymw.Client)
 
@@ -104,89 +99,7 @@ func main() {
 		})
 	})
 
-	r.Get("/auth", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, config.GoogleOAuth2.AuthCodeURL("", oauth2.AccessTypeOffline, oauth2.ApprovalForce), http.StatusFound)
-	})
-
-	r.Get("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		queries := yikes.New(conn)
-
-		code := r.URL.Query().Get("code")
-		token, err := config.GoogleOAuth2.Exchange(ctx, code)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		client := config.GoogleOAuth2.Client(ctx, token)
-
-		resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		bytes, err := io.ReadAll(resp.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		userInfo := struct {
-			Email string `json:"email"`
-		}{}
-		json.Unmarshal(bytes, &userInfo)
-
-		user, err := queries.FindUserByEmail(ctx, userInfo.Email)
-		if err != nil {
-			if errors.Is(err, pgx.ErrNoRows) {
-				user, err = queries.CreateUser(ctx, userInfo.Email)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-			} else {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-		}
-		if user.Email == "" {
-			panic("EMPTY EMAIL")
-		}
-
-		bytes, err = json.Marshal(token)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		_, err = queries.CreateToken(ctx, yikes.CreateTokenParams{Token: bytes, UserID: user.ID})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		sessionID, err := queries.CreateSession(ctx, user.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		value, err := sessionID.Value()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:    "yikes.session",
-			Value:   value.(string),
-			Expires: time.Now().Add(30 * 24 * time.Hour),
-			Path:    "/",
-		})
-
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	})
+	r.Route("/auth", auth.Router)
 
 	// Start the server
 	log.Println("Server started on :8080")
---
+++ routes/auth/router.go
@@ -0,0 +1,106 @@
+package auth
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"time"
+	"yikes/config"
+	"yikes/db/yikes"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
+	"golang.org/x/oauth2"
+)
+
+func Router(r chi.Router) {
+	r.Get("/auth", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, config.GoogleOAuth2.AuthCodeURL("", oauth2.AccessTypeOffline, oauth2.ApprovalForce), http.StatusFound)
+	})
+
+	r.Get("/auth/callback", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		conn, ok := ctx.Value("conn").(*pgx.Conn)
+		if !ok {
+			http.Error(w, "Client: couldn't get conn", http.StatusInternalServerError)
+			return
+		}
+		queries := yikes.New(conn)
+
+		code := r.URL.Query().Get("code")
+		token, err := config.GoogleOAuth2.Exchange(ctx, code)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := config.GoogleOAuth2.Client(ctx, token)
+
+		resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		bytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		userInfo := struct {
+			Email string `json:"email"`
+		}{}
+		json.Unmarshal(bytes, &userInfo)
+
+		user, err := queries.FindUserByEmail(ctx, userInfo.Email)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				user, err = queries.CreateUser(ctx, userInfo.Email)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+			} else {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+		if user.Email == "" {
+			panic("EMPTY EMAIL")
+		}
+
+		bytes, err = json.Marshal(token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		_, err = queries.CreateToken(ctx, yikes.CreateTokenParams{Token: bytes, UserID: user.ID})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		sessionID, err := queries.CreateSession(ctx, user.ID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		value, err := sessionID.Value()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:    "yikes.session",
+			Value:   value.(string),
+			Expires: time.Now().Add(30 * 24 * time.Hour),
+			Path:    "/",
+		})
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	})
+}
