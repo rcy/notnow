7c15ebdf3f77961387748dc605ce4668b66cf908
Ryan Yeske <ryan@ryanyeske.com>
2023-11-12 21:28:51 -0700

    Move Event to its own file

--- services/google/events.go
+++ services/google/events.go
@@ -19,65 +19,6 @@ const (
 	contextPrefix = "@"
 )
 
-type Event struct {
-	calendar.Event
-}
-
-func (e *Event) AllDay() bool {
-	return e.Start.DateTime == ""
-}
-
-func (e *Event) StartDate() string {
-	if e.Start.Date != "" {
-		return e.Start.Date
-	}
-	return e.StartAt().Format(time.DateOnly)
-}
-
-func (e *Event) StartTime() string {
-	if e.Start.Date != "" {
-		return ""
-	}
-	return e.StartAt().Format("15:04")
-}
-
-func (e *Event) EndTime() string {
-	if e.End.Date != "" {
-		return ""
-	}
-	return e.EndAt().Format("15:04")
-}
-
-func (e *Event) StartAt() time.Time {
-	if e.Start.DateTime == "" {
-		t, _ := time.Parse(time.DateOnly, e.Start.Date)
-		return t
-	}
-	t, _ := time.Parse(time.RFC3339Nano, e.Start.DateTime)
-	return t
-}
-
-func (e *Event) EndAt() time.Time {
-	if e.End.DateTime == "" {
-		t, _ := time.Parse(time.DateOnly, e.End.Date)
-		return t
-	}
-	t, _ := time.Parse(time.RFC3339Nano, e.End.DateTime)
-	return t
-}
-
-func (e *Event) Duration() time.Duration {
-	return e.EndAt().Sub(e.StartAt())
-}
-
-func (e *Event) IsTask() bool {
-	return strings.HasPrefix(e.Summary, taskPrefix)
-}
-
-func (e *Event) IsContext() bool {
-	return strings.HasPrefix(e.Summary, contextPrefix)
-}
-
 type fetchEventsParam struct {
 	Min time.Time
 	Max time.Time
